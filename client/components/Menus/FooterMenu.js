ec20f5c77cc4fc705c313ce71e2da1adaf4fc50e
minhbro3003 <minhtiencool03@gmail.com>
2025-01-24 00:33:34 +0700

    Design Footer Menu

--- client/components/Menus/FooterMenu.js
+++ client/components/Menus/FooterMenu.js
@@ -1,15 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 const FooterMenu = () => {
     return (
-        <View>
-            <Text>Hello</Text>
-            <View>Hello</View>
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <FontAwesome5 name="home" style={styles.iconStyle} />
+                <Text>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <FontAwesome5 name="plus-square" style={styles.iconStyle} />
+                <Text>Post</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <FontAwesome5 name="info-circle" style={styles.iconStyle} />
+                <Text>About</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <FontAwesome5 name="user" style={styles.iconStyle} />
+                <Text>Acount</Text>
+            </TouchableOpacity>
         </View>
     );
 };
 
 export default FooterMenu;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        margin: 10,
+        justifyContent: "space-between",
+    },
+    iconStyle: {
+        marginBottom: 3,
+        alignSelf: "center",
+        fontSize: 20,
+    },
+});
